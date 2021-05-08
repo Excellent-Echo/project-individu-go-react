5936c9a1bb9f5a0a056705b531c8ecdc68cbbc1d
mohammedazez <muhamadaziz047@gmail.com>
2021-05-08 23:14:50 +0700

    Clean Arcitecture table user for Get user by id and delete user by id

--- handler/user.go
+++ handler/user.go
@@ -10,30 +10,6 @@ import (
 
 var DB = config.ConnectToDatabase()
 
-func GetUserByID(c *gin.Context) {
-	var GetUserID entity.User
-
-	id := c.Param("user_id")
-
-	if err := DB.Where("id = ?", id).Preload("Booking").Find(&GetUserID).Error; err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"status":        "error bad request",
-			"message_error": err.Error(),
-		})
-		return
-	}
-
-	if GetUserID.ID == 0 {
-		c.JSON(http.StatusNotFound, gin.H{
-			"status":        "error not found",
-			"message_error": "user id " + id + " not found in database",
-		})
-		return
-	}
-
-	c.JSON(http.StatusOK, GetUserID)
-}
-
 func UpdateUserByID(c *gin.Context) {
 	var updateUser entity.User
 	var updateUserInput entity.UserInput
@@ -80,33 +56,4 @@ func UpdateUserByID(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, updateUser)
-}
-
-func DeleteByUserID(c *gin.Context) {
-	id := c.Param("user_id")
-
-	var DeleteUser entity.User
-
-	DB.Where("id = ?", id).Find(&DeleteUser)
-
-	if DeleteUser.ID == 0 {
-		c.JSON(http.StatusNotFound, gin.H{
-			"status":        "error not found",
-			"message_error": "user id " + id + " not found in database",
-		})
-		return
-	}
-
-	if err := DB.Where("id = ?", id).Delete(&entity.User{}).Error; err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"status":        "error in internal server",
-			"message_error": err.Error(),
-		})
-		return
-	}
-
-	c.JSON(http.StatusOK, gin.H{
-		"status":  "delete success",
-		"message": "user id " + id + " successfull delete",
-	})
 }
