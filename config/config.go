610640020c280b04886e1d0b0c7519687350a561
ammarteuku05 <ammarteuku09@gmail.com>
2021-05-11 23:12:26 +0700

    config use env selesai

--- config/config.go
+++ config/config.go
@@ -1,22 +1,34 @@
 package config
 
 import (
-	"projectpenyewaanlapangan/entity"
+	"fmt"
+	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func Connect() *gorm.DB {
-	dsn := "root:@tcp(localhost)/penyewaanlapangan?charset=utf8mb4&parseTime=True&loc=Local"
+
+	err := godotenv.Load()
+
+	dbUser := os.Getenv("DB_USERNAME")
+	dbPass := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbName := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPass, dbHost, dbName)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	// dsn := "root:@tcp(localhost)/penyewaanlapangan?charset=utf8mb4&parseTime=True&loc=Local"
+	// db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		panic(err.Error())
 	}
 
-	db.AutoMigrate(&entity.User{})
-	db.AutoMigrate(&entity.SportList{})
+	// db.AutoMigrate(&entity.User{})
+	// db.AutoMigrate(&entity.SportList{})
 
 	return db
 }
