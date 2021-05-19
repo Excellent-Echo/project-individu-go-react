cfcd16384160e990297ba9639ac0d2cd634e9a63
mohammedazez <muhamadaziz047@gmail.com>
2021-05-19 10:43:57 +0700

    add environment

--- config/config.go
+++ config/config.go
@@ -1,13 +1,23 @@
 package config
 
 import (
+	"fmt"
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"os"
 )
 
 // ConnectToDatabase untuk koneksi dari database MySQL
 func ConnectToDatabase() *gorm.DB {
-	dsn := "root:root@tcp(localhost)/konsultasi_psikolog?charset=utf8mb4&parseTime=True&loc=Local"
+	err := godotenv.Load()
+
+	dbUser := os.Getenv("DB_USERNAME")
+	dbPass := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbName := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPass, dbHost, dbName)
 
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
