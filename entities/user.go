5e7e8925acc8d77de5335553d73b553cf3399961
ardhisr <radika.kerenz1@gmail.com>
2021-05-22 00:27:16 +0700

    add user crud repo

--- entities/user.go
+++ entities/user.go
@@ -11,3 +11,16 @@ type User struct {
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
+
+type UserInput struct {
+	FirstName string `json:"first_name" binding:"required"`
+	LastName  string `json:"last_name" binding:"required"`
+	Email     string `json:"email" binding:"required,email"`
+	Password  string `json:"password" binding:"required"`
+}
+
+type UpdateUserInput struct {
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email"`
+}
--- user/repository.go
+++ user/repository.go
@@ -8,7 +8,10 @@ import (
 
 type Repository interface {
 	FindAll() ([]entities.User, error)
-	//Create(user entities.User) (entities.User, error)
+	Create(user entities.User) (entities.User, error)
+	UpdateByID(ID string, dataUpdate map[string]interface{}) (entities.User, error)
+	DeleteByID(ID string) (string, error)
+	FindByID(ID string) (entities.User, error)
 }
 
 type repository struct {
@@ -19,6 +22,7 @@ func NewRepository(db *gorm.DB) *repository {
 	return &repository{db}
 }
 
+//Read
 func (r *repository) FindAll() ([]entities.User, error) {
 	var users []entities.User
 
@@ -27,4 +31,39 @@ func (r *repository) FindAll() ([]entities.User, error) {
 	}
 
 	return users, nil
+}
+
+//Read by ID
+func (r *repository) FindByID(ID string) (entities.User, error) {
+	var user entities.User
+
+	if err := r.db.Where("id = ?", ID).Find(&user).Error; err != nil {
+		return user, err
+	}
+
+	return user, nil
+}
+
+//Update
+func (r *repository) UpdateByID(ID string, dataUpdate map[string]interface{}) (entities.User, error) {
+	var user entities.User
+
+	if err := r.db.Model(&user).Where("id = ?", ID).Updates(dataUpdate).Error; err != nil {
+		return user, err
+	}
+
+	if err := r.db.Where("id = ?", ID).Find(&user).Error; err != nil {
+		return user, err
+	}
+
+	return user, nil
+}
+
+//Delete
+func (r *repository) DeleteByID(ID string) (string, error) {
+	if err := r.db.Where("id=?", ID).Delete(&entities.User{}).Error; err != nil {
+		return "error", err
+	}
+
+	return "delete success", nil
 }
