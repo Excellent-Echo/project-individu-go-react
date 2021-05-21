008c5df6fd977b4bf9ede7ca306d830875288fed
rifqi23ahmad <rifqi23ahmad@gmail.com>
2021-05-22 00:05:41 +0700

    entity proses

--- entity/entity.go
+++ entity/entity.go
@@ -11,6 +11,27 @@ type User struct {
 	DeleteAt  time.Time `gorm:"index"`
 }
 
-type M021 struct {
+type Mentor struct {
 	ID int `gorm:"primaryKey"`
+	FirstName string
+	LastName  string
+	Email     string
+	Password  string
+	CreateAt  time.Time
+	UpdateAt  time.Time
+	DeleteAt  time.Time `gorm:"index"`
+}
+
+type Booking struct {
+	ID int `gorm:"primaryKey"`
+	ClassProgram string
+	BookingDate time.Time
+	DurasiJam int 
+
+}
+
+type Role struct {
+	ID int `gorm:"primarykey"`
+	LoginAs string
+	LoginDate time.Time
 }
