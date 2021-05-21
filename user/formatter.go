095618dbcb8c7595755256be24fa4593d1c1fe09
ardhisr <radika.kerenz1@gmail.com>
2021-05-21 23:26:00 +0700

    add more format

--- user/formatter.go
+++ user/formatter.go
@@ -1,8 +1,21 @@
 package user
 
+import "project-individu-go-react/entities"
+
 type UserFormat struct {
 	ID        int    `json:"id"`
 	FirstName string `json:"first_name"`
 	LastName  string `json:"last_name"`
 	Email     string `json:"email"`
+}
+
+func FormatUser(user entities.User) UserFormat {
+	var formatUser = UserFormat{
+		ID:        user.ID,
+		FirstName: user.FirstName,
+		LastName:  user.LastName,
+		Email:     user.Email,
+	}
+
+	return formatUser
 }
