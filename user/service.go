f738733a1149bc661726791e3c6de2d3c482dd3f
ardhisr <radika.kerenz1@gmail.com>
2021-05-21 23:37:19 +0700

    add user service

---
+++ user/service.go
@@ -0,0 +1,30 @@
+package user
+
+type Service interface {
+	GetAllUser() ([]UserFormat, error)
+}
+
+type service struct {
+	repository Repository
+}
+
+func NewService(repo Repository) *service {
+	return &service{repo}
+}
+
+func (s *service) GetAllUser() ([]UserFormat, error) {
+	//Logic
+	users, err := s.repository.FindAll()
+	var formatUsers []UserFormat
+
+	for _, user := range users {
+		formatUser := FormatUser(user)
+		formatUsers = append(formatUsers, formatUser)
+	}
+
+	if err != nil {
+		return formatUsers, err
+	}
+
+	return formatUsers, nil
+}
