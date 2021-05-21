90d7ccfe2ebca3c972efeda9ea2ef38f918c5f19
ardhisr <radika.kerenz1@gmail.com>
2021-05-21 23:19:59 +0700

    make format for user

---
+++ user/formatter.go
@@ -0,0 +1,8 @@
+package user
+
+type UserFormat struct {
+	ID        int    `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email"`
+}
