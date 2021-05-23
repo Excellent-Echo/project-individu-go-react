74b68cf86d1ede5118cbee31dbc7e81f1f7824c9
rifqi23ahmad <rifqi23ahmad@gmail.com>
2021-05-23 13:15:13 +0700

    :(

--- entity/entity.go
+++ entity/entity.go
@@ -1,41 +1,43 @@
 package entity
 
-import "time"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type User struct {
-	ID        int `gorm:"primaryKey"`
+	gorm.Model
 	FirstName string
 	LastName  string
 	Email     string
 	Password  string
-	CreateAt  time.Time
-	UpdateAt  time.Time
-	DeleteAt  time.Time `gorm:"index"`
-	// Booking   []Booking `gorm:"foreignKey:UserID"`
-	// Mentor    []Mentor  `gorm:"foreignKey:UserID"`
-	// Role      []Role    `gorm:"foreignKey:UserID"`
-}
-
-type Mentor struct {
-	ID        int `gorm:"primaryKey"`
-	FirstName string
-	LastName  string
-	Email     string
-	Password  string
-	CreateAt  time.Time
-	UpdateAt  time.Time
-	DeleteAt  time.Time `gorm:"index"`
-}
-
-type Booking struct {
-	ID           int `gorm:"primaryKey"`
-	ClassProgram string
-	BookingDate  time.Time
-	DurasiJam    int
+	Role      []Role `gorm:"foreignKey:UserID"`
 }
 
 type Role struct {
-	ID        int `gorm:"primarykey"`
-	LoginAs   string
-	LoginDate time.Time `gorm:"index"`
+	ID          int `gorm:"primaryKey"`
+	Title       string
+	Description string
+}
+
+type Student struct {
+	ID int `gorm:"primaryKey"`
+	// Mentor  []Mentor  `gorm:"foreignKey:MentorID"`
+	// Booking []Booking `gorm:"foreignKey:BookingID"`
+}
+
+type Mentor struct {
+	ID int `gorm:"primaryKey"`
+	// User    []User    `gorm:"foreignKey:UserID"`
+	// Booking []Booking `gorm:"foreignKey:BookingID"`
+}
+
+type Booking struct {
+	ID          int `gorm:"primaryKey"`
+	Title       string
+	Description string
+	Time        time.Time
+	Date        time.Time
+	IsCompleted bool
 }
