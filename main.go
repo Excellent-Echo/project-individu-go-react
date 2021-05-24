e98bbd1b7f3f0354a2a519aecd95020fdc2b0833
mohammedazez <muhamadaziz047@gmail.com>
2021-05-24 22:00:01 +0700

    Clean Code Route in main

--- main.go
+++ main.go
@@ -2,72 +2,18 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"project-individu-go-react/config"
-	"project-individu-go-react/handler"
-	"project-individu-go-react/layer/psikolog"
-	"project-individu-go-react/layer/user"
-)
-
-// Database, Repository, Service dan Handler
-var (
-	DB             = config.ConnectToDatabase()
-	userRepository = user.NewRepository(DB)
-	userService    = user.NewService(userRepository)
-	userHandler    = handler.NewUserHander(userService)
-
-	psikologRepository = psikolog.NewRepository(DB)
-	psikologService    = psikolog.NewService(psikologRepository)
-	psikologHandler    = handler.NewPsikologHandler(psikologService)
+	"project-individu-go-react/routes"
 )
 
 func main() {
-	//var booking []entity.Booking
-	//
-	//if err := DB.Find(&booking).Error ; err != nil {
-	//	panic(err.Error())
-	//}
-	//
-	//fmt.Println(booking)
-
 	router := gin.Default()
 
-	// Endpoint untuk model users
-	router.GET("/users", userHandler.ShowUserHandler)
-	router.POST("/users/register", userHandler.CreateUserHandler)
-	router.GET("users/:user_id", userHandler.GetUserByIDHandler)
-	router.PUT("users/:user_id", userHandler.GetandUpdateUserByIDHandler)
-	router.DELETE("users/:user_id", userHandler.GetandDeleteUserByIDHandler)
-	//router.POST("/users/login", userHandler.CreateUserHandler)
+	routes.UserRoute(router)
+	routes.PsikologRoute(router)
+	routes.RoleRoute(router)
+	routes.BookingDetailRoute(router)
+	routes.BookingRoute(router)
 
-	// Endpoint untuk model psikolog
-	router.GET("/psikologs", psikologHandler.ShowPsikologHandler)
-	router.POST("/psikologs", psikologHandler.CreatePsikologHandler)
-	router.GET("psikologs/:psikolog_id", psikologHandler.GetPsikologByIDHandler)
-	router.PUT("psikologs/:psikolog_id", psikologHandler.UpdatePsikologByIDHandler)
-	router.DELETE("psikologs/:psikolog_id", psikologHandler.DeletePsikologByIDHandler)
-
-	// Endpoint untuk model role
-	router.GET("/roles", handler.GetAllRole)
-	router.POST("/roles", handler.CreateNewRole)
-	router.GET("roles/:role_id", handler.GetRoleByID)
-	router.PUT("roles/:role_id", handler.UpdateRoleByID)
-	router.DELETE("roles/:role_id", handler.DeleteByRoleID)
-
-	// Endpoint untuk model booking
-	router.GET("/booking", handler.GetAllBooking)
-	router.POST("/booking", handler.CreateNewBooking)
-	router.GET("booking/:booking_id", handler.GetBookingByID)
-	router.PUT("booking/:booking_id", handler.UpdateBookingByID)
-	router.DELETE("booking/:booking_id", handler.DeleteByBookingID)
-
-	// Endpoint untuk model booking detail
-	router.GET("/booking-detail", handler.GetAllBookingDetail)
-	router.POST("/booking-detail", handler.CreateNewBookingDetail)
-	router.GET("booking-detail/:booking_detail_id", handler.GetBookingDetailByID)
-	router.PUT("booking-detail/:booking-detail_id", handler.UpdateBookingDetailByID)
-	router.DELETE("booking-detail/:booking_detail_id", handler.DeleteByBookingDetailID)
-
-	// Untuk menjalanakan server ke localhost komputer
 	router.Run(":3000")
 
 }
