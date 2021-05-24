c995ad14d99680cb6cd16ade625f2017328be440
ammarteuku05 <ammarteuku09@gmail.com>
2021-05-24 16:01:11 +0700

    update clean code routes untuk user dan sportlist selesai

--- main.go
+++ main.go
@@ -1,27 +1,9 @@
 package main
 
 import (
-	"projectpenyewaanlapangan/config"
-	"projectpenyewaanlapangan/handler"
 	"projectpenyewaanlapangan/routes"
-	"projectpenyewaanlapangan/sportlist"
 
 	"github.com/gin-gonic/gin"
-	"gorm.io/gorm"
-)
-
-var (
-	DB *gorm.DB = config.Connect()
-
-	// user entity
-	// userRepository = user.NewRepository(DB)
-	// userService    = user.NewService(userRepository)
-	// userHandler    = handler.NewUserHandler(userService)
-
-	//sportlsit entity
-	sportlistRepository = sportlist.NewRepository(DB)
-	sportlistService    = sportlist.NewService(sportlistRepository)
-	sportlistHandler    = handler.NewSportListHandler(sportlistService)
 )
 
 func main() {
