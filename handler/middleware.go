de3fdf705f2b75a6bff02e76f1fa2d7f229d5bd2
mohammedazez <muhamadaziz047@gmail.com>
2021-05-24 22:05:13 +0700

    Middleware for users GET, PUT, DELETE

---
+++ handler/middleware.go
@@ -0,0 +1,41 @@
+package handler
+
+import (
+	"project-individu-go-react/auth"
+	"project-individu-go-react/helper"
+	"project-individu-go-react/layer/user"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+)
+
+func Middleware(userService user.Service, authService auth.Service) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+
+		if authHeader == "" || len(authHeader) == 0 {
+			errorResponse := helper.APIResponse("Unauthorize", 401, "error", gin.H{"error": "unauthorize user"})
+			c.AbortWithStatusJSON(401, errorResponse)
+			return
+		}
+
+		token, err := authService.ValidateToken(authHeader)
+
+		if err != nil {
+			errorResponse := helper.APIResponse("Unauthorize", 401, "error", gin.H{"error": err.Error()})
+			c.AbortWithStatusJSON(401, errorResponse)
+			return
+		}
+
+		claim, ok := token.Claims.(jwt.MapClaims)
+
+		if !ok {
+			errorResponse := helper.APIResponse("Unauthorize", 401, "error", gin.H{"error": "unauthorize user"})
+			c.AbortWithStatusJSON(401, errorResponse)
+			return
+		}
+
+		userID := int(claim["user_id"].(float64))
+		c.Set("currentUser", userID)
+	}
+}
