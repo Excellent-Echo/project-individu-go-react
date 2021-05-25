bf37177449872bdf494a4d41588333cb480f1d17
mohammedazez <muhamadaziz047@gmail.com>
2021-05-25 11:25:51 +0700

    without env

--- config/config.go
+++ config/config.go
@@ -1,23 +1,23 @@
 package config
 
 import (
-	"fmt"
-	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
-	"os"
+	"project-individu-go-react/migration"
 )
 
 // ConnectToDatabase untuk koneksi dari database MySQL
 func ConnectToDatabase() *gorm.DB {
-	err := godotenv.Load()
+	//err := godotenv.Load()
+	//
+	//dbUser := os.Getenv("DB_USERNAME")
+	//dbPass := os.Getenv("DB_PASSWORD")
+	//dbHost := os.Getenv("DB_HOST")
+	//dbName := os.Getenv("DB_NAME")
 
-	dbUser := os.Getenv("DB_USERNAME")
-	dbPass := os.Getenv("DB_PASSWORD")
-	dbHost := os.Getenv("DB_HOST")
-	dbName := os.Getenv("DB_NAME")
+	//dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPass, dbHost, dbName)
 
-	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPass, dbHost, dbName)
+	dsn := "root:root@tcp(localhost)/konsultasi_psikolog?charset=utf8mb4&parseTime=True&loc=Local"
 
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
@@ -26,11 +26,11 @@ func ConnectToDatabase() *gorm.DB {
 	}
 
 	// Untuk membuat tabel baru ke database MySQL
-	//db.AutoMigrate(&entity.User{})
-	//db.AutoMigrate(&entity.Role{})
-	//db.AutoMigrate(&entity.Booking{})
-	//db.AutoMigrate(&entity.BookingDetail{})
-	//db.AutoMigrate(&entity.Psikologi{})
+	db.AutoMigrate(&migration.User{})
+	db.AutoMigrate(&migration.Role{})
+	db.AutoMigrate(&migration.Booking{})
+	db.AutoMigrate(&migration.BookingDetail{})
+	db.AutoMigrate(&migration.Psikologi{})
 
 	return db
 }
