dd78650511df59a310e9ab0ca5e43af5153d10e2
marwanjuna <mrwnolii@gmail.com>
2021-05-25 11:05:05 +0700

    fix bug?

--- entity/tags.go
+++ entity/tags.go
@@ -1,8 +1,10 @@
 package entity
 
+import "time"
+
 type Tags struct {
-	ID  uint32 `gorm:"primaryKey;autoIncrement;not null" json:"id"`
-	Tag string `gorm:"size:255;not null" json:"tag"`
-	// CreatedAt time.Time   `gorm:"type:datetime;not null;default:current_timestamp" json:"-"`
+	ID        uint32      `gorm:"primaryKey;autoIncrement;not null" json:"id"`
+	Tag       string      `gorm:"size:255;not null" json:"tag"`
+	CreatedAt time.Time   `gorm:"type:datetime;not null;default:current_timestamp" json:"-"`
 	Questions []Questions `gorm:"many2many:question_tags" json:"questions"`
 }
