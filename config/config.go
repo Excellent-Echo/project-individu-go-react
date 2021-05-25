09c45ace861bfa2a4b73c049ee705d566290ae11
helyus1412 <helyus1412@gmail.com>
2021-05-25 11:07:22 +0700

    add env heroku

--- config/config.go
+++ config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"fmt"
 	"os"
+	"project-individu-go-react/entity"
 
 	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
@@ -18,19 +19,20 @@ func Connection() *gorm.DB {
 	dbUser := os.Getenv("DB_USERNAME")
 	dbPass := os.Getenv("DB_PASSWORD")
 	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
 	dbName := os.Getenv("DB_NAME")
 
-	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPass, dbHost, dbName)
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPass, dbHost, dbPort, dbName)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		panic(err.Error())
 	}
 
-	// db.AutoMigrate(&entity.User{})
-	// db.AutoMigrate(&entity.Category{})
-	// db.AutoMigrate(&entity.UserDetail{})
-	// db.AutoMigrate(&entity.Course{})
+	db.AutoMigrate(&entity.User{})
+	db.AutoMigrate(&entity.Category{})
+	db.AutoMigrate(&entity.UserDetail{})
+	db.AutoMigrate(&entity.Course{})
 
 	return db
 
