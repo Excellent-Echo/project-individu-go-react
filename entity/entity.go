5e01f54f7e0a72c1ac66f9184c815bb4c14c3fbc
andryansyhh <andryansyhh@gmail.com>
2021-05-26 22:54:55 +0700

    berhasil nge post

--- entity/entity.go
+++ entity/entity.go
@@ -1,28 +1,31 @@
 package entity
 
-import "time"
+import (
+	"gorm.io/gorm"
+	"time"
+)
 
 type User struct {
-	ID        int `gorm:"primaryKey"`
-	FirstName string
-	LastName  string
-	Email     string
-	Password  string
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	DeletedAt time.Time `gorm:"index"`
+	ID        int            `gorm:"primaryKey" json:"id`
+	FirstName string         `json:"first_name"`
+	LastName  string         `json:"last_name"`
+	Email     string         `json:"email"`
+	Password  string         `json:"password"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"update_at"`
+	DeletedAt gorm.DeletedAt `json:"deleted_at"`
 }
 
 type Product struct {
-	ID          int    `gorm:"primaryKey"`
-	ProductName string ``
-	Quantity    string
+	ID          int    `gorm:"primaryKey" json:"id`
+	ProductName string `json:"product_name"`
+	Quantity    string `json:"quantity"`
 	//User 		[]User `gorm:"foreignKey:ProductID"`
 }
 
 type Order struct {
-	ID         int `gorm:"primaryKey"`
-	OrderDate  uint
-	ShipAdress string
+	ID         int    `gorm:"primaryKey" json:"order_id"`
+	OrderDate  uint   `json:"order_date"`
+	ShipAdress string `json:"ship_adress"`
 	//Product []Product `gorm:foreignKey:OrderID`
 }
