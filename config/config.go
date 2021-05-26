09b2d0012f9fc0cbf4b4bd98673f6bf3d81a6ffb
ammarteuku05 <ammarteuku09@gmail.com>
2021-05-26 20:47:13 +0700

    deploy with heroku

--- config/config.go
+++ config/config.go
@@ -1,8 +1,7 @@
 package config
 
 import (
-	"fmt"
-	"os"
+	"projectpenyewaanlapangan/entity"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -12,20 +11,23 @@ func Connect() *gorm.DB {
 
 	// err := godotenv.Load()
 
-	dbUser := os.Getenv("DB_USERNAME")
-	dbPass := os.Getenv("DB_PASSWORD")
-	dbHost := os.Getenv("DB_HOST")
-	dbName := os.Getenv("DB_NAME")
+	// dbUser := os.Getenv("DB_USERNAME")
+	// dbPass := os.Getenv("DB_PASSWORD")
+	// dbHost := os.Getenv("DB_HOST")
+	// dbName := os.Getenv("DB_NAME")
 
-	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPass, dbHost, dbName)
+	// dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPass, dbHost, dbName)
+	// db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	dsn := "qZt7K9iV7Q:a8EVx6Tf1w@tcp(remotemysql.com:3306)/qZt7K9iV7Q?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		panic(err.Error())
 	}
 
-	// db.AutoMigrate(&entity.User{})
-	// db.AutoMigrate(&entity.SportList{})
+	db.AutoMigrate(&entity.User{})
+	db.AutoMigrate(&entity.SportList{})
 
 	return db
 }
