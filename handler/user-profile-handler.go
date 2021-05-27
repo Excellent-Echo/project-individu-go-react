5d67cb685a84bfb0c08da4243334e7353201c64d
marwanjuna <mrwnolii@gmail.com>
2021-05-27 11:12:21 +0700

    add method get & post upload user profile

---
+++ handler/user-profile-handler.go
@@ -0,0 +1,74 @@
+package handler
+
+import (
+	"fmt"
+	"project-individu-go-react/helper"
+	"project-individu-go-react/layer/userProfile"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+type userProfileHandler struct {
+	service userProfile.Service
+}
+
+func NewUserProfileHandler(service userProfile.Service) *userProfileHandler {
+	return &userProfileHandler{service}
+}
+
+func (h *userProfileHandler) GetUserProfileByUserIDHandler(c *gin.Context) {
+	userData := int(c.MustGet("currentUser").(int))
+
+	userID := strconv.Itoa(userData)
+
+	userProfile, err := h.service.GetUserProfile(userID)
+
+	if err != nil {
+		responseError := helper.APIResponse("status unauthorize", 401, "error", gin.H{"error": err.Error()})
+
+		c.JSON(401, responseError)
+		return
+	}
+
+	response := helper.APIResponse("success get user profile by user ID", 200, "success", userProfile)
+	c.JSON(200, response)
+}
+
+func (h *userProfileHandler) SaveNewUserProfileHandler(c *gin.Context) {
+
+	userData := int(c.MustGet("currentUser").(int))
+
+	file, err := c.FormFile("profile") // postman
+
+	if err != nil {
+		responseError := helper.APIResponse("status bad request", 400, "error", gin.H{"error": err.Error()})
+
+		c.JSON(400, responseError)
+		return
+	}
+
+	path := fmt.Sprintf("images/profile-%d-%s", userData, file.Filename)
+
+	err = c.SaveUploadedFile(file, path)
+
+	if err != nil {
+		// log.Println("error line 63")
+		responseError := helper.APIResponse("status bad request", 400, "error", gin.H{"error": err.Error()})
+
+		c.JSON(400, responseError)
+		return
+	}
+
+	userProfile, err := h.service.SaveNewUserProfile(path, userData)
+
+	if err != nil {
+		responseError := helper.APIResponse("Internal server error", 500, "error", gin.H{"error": err.Error()})
+
+		c.JSON(500, responseError)
+		return
+	}
+
+	response := helper.APIResponse("success create user profile", 201, "success", userProfile)
+	c.JSON(201, response)
+}
