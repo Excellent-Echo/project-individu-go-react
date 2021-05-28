3c551201c97e0e773151ccf3cfe76ff3f2ab4acb
ammarteuku05 <ammarteuku09@gmail.com>
2021-05-28 10:36:23 +0700

    update filedlist

--- routes/fieldlist.go
+++ routes/fieldlist.go
@@ -1,21 +1,13 @@
 package routes
 
-import (
-	"projectpenyewaanlapangan/fieldlist"
-	"projectpenyewaanlapangan/handler"
+// var (
+// 	fieldlistRepository = fieldlist.NewRepository(DB)
+// 	fieldlistService = fieldlist.NewService(fieldlistRepository)
+// 	fieldlistHandler = handler.NewFieldListHandler(fieldlistService)
+// )
 
-	"github.com/gin-gonic/gin"
-)
-
-
-var (
-	fieldlistRepository = fieldlist.NewRepository(DB)
-	fieldlistService = fieldlist.NewService(fieldlistRepository)
-	fieldlistHandler = handler.NewFieldListHandler(fieldlistService)
-)
-
-func FieldListRoutes(r *gin.Engine) {
-	r.GET("/fieldlist", fieldlistHandler.showFieldListHandler)
-	r.POST("/fieldlist/register", fieldlistHandler.CreateFieldListHandler)
-	r.GET("/fieldlist/:fieldlistbyid", fieldlistHandler.)
-}
+// func FieldListRoutes(r *gin.Engine) {
+// 	r.GET("/fieldlist", fieldlistHandler.showFieldListHandler)
+// 	r.POST("/fieldlist/register", fieldlistHandler.CreateFieldListHandler)
+// 	r.GET("/fieldlist/:fieldlistbyid", fieldlistHandler.)
+// }
