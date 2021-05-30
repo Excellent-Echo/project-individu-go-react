a4a4628d4aac0f07f0ad2b4338d1e6090e186043
marwanjuna <mrwnolii@gmail.com>
2021-05-30 22:20:30 +0700

    update formatting questions

--- layer/question/question-formatter.go
+++ layer/question/question-formatter.go
@@ -3,25 +3,23 @@ package question
 import "project-individu-go-react/entity"
 
 type QuestionFormat struct {
-	ID           uint64      `json:"id"`
-	Title        string      `json:"title"`
-	Content      string      `json:"content"`
-	UserID       uint32      `json:"user_id"`
-	User         entity.User `json:"user_detail"`
-	CategoryID   uint32      `json:"category_id"`
-	CategoryName string      `json:"category_name"`
-	// Tags    []entity.Tags    `json:"tags"`
-	Answers []entity.Answers `json:"answers"`
+	ID           uint64           `json:"id"`
+	Title        string           `json:"title"`
+	Content      string           `json:"content"`
+	UserID       uint32           `json:"user_id"`
+	UserName     string           `json:"user_name"`
+	CategoryID   uint32           `json:"category_id"`
+	CategoryName string           `json:"category_name"`
+	Answers      []entity.Answers `json:"answers"`
 }
 
 func FormattingQuestion(question entity.Questions) QuestionFormat {
 	questionFormat := QuestionFormat{
-		ID:      question.ID,
-		Title:   question.Title,
-		Content: question.Content,
-		UserID:  question.UserID,
-		User:    question.User,
-		// Tags:    question.Tags,
+		ID:           question.ID,
+		Title:        question.Title,
+		Content:      question.Content,
+		UserID:       question.UserID,
+		UserName:     question.User.UserName,
 		CategoryID:   question.CategoryID,
 		CategoryName: question.Category.CategoryName,
 		Answers:      question.Answers,
