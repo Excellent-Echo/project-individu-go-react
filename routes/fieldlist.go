0174e33bd0b4ecd39a107f8ceea2a252a081a704
ammarteuku05 <ammarteuku09@gmail.com>
2021-06-01 23:16:20 +0700

    update routes

--- routes/fieldlist.go
+++ routes/fieldlist.go
@@ -17,5 +17,5 @@ func FieldListRoutes(r *gin.Engine) {
 	r.GET("/fieldlist", fieldlistHandler.ShowFieldListHandler)
 	r.POST("/fieldlist", fieldlistHandler.SaveNewFieldListHandler)
 	r.GET("/fieldlist/:field_id", fieldlistHandler.GetFieldListByID)
-	r.PUT("/fieldlist/:field_id")
+	r.PUT("/fieldlist/:field_id", fieldlistHandler.UpdateFieldListByIdHandler)
 }
