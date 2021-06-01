513f663c78c4f1acd7e455df6427f99577f52df8
ammarteuku05 <ammarteuku09@gmail.com>
2021-06-01 21:44:45 +0700

    update bookinglist entity

--- entity/bookinglist.go
+++ entity/bookinglist.go
@@ -3,19 +3,19 @@ package entity
 import "time"
 
 type BookingList struct {
-	ID        int       `gorm:"primaryKey" json:"id"`
-	Date      time.Time `json:"date"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	ID          int       `gorm:"primaryKey" json:"id"`
+	Date        time.Time `json:"date"`
+	UserID      int       `json:"user_id"`
+	FieldID     int       `json:"field_id"`
+	TimeForPlay int       `json:"time_for_play"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
 	// DeletedAt time.Time `gorm:"index" json:"-"`
-	User_id   int    `json:"userID"`
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
 }
 
 type BookingListInput struct {
-	Date      time.Time `json:"date"`
-	User_id   int       `json:"userID"`
-	FirstName string    `json:"first_name"`
-	LastName  string    `json:"last_name"`
+	Date        time.Time `json:"date"`
+	UserID      int       `json:"user_id"`
+	FieldID     int       `json:"field_id"`
+	TimeForPlay int       `json:"time_for_play"`
 }
